e5e0aaf6d215458c1caffb0075d65932fb054350
vvvvv <vvvvv@vvvvv.io>
2022-11-15 16:41:08 +0100

    added Wno-unused-but-set-variable so harfbuzz builds on arm macos

--- esy/configure-osx.sh
+++ esy/configure-osx.sh
@@ -1,2 +1,2 @@
 cd _build
-./configure --enable-shared=no --enable-static=yes --with-pic=yes --prefix=$cur__install
+CFLAGS=-Wno-unused-but-set-variable ./configure --enable-shared=no --enable-static=yes --with-pic=yes --prefix=$cur__install
